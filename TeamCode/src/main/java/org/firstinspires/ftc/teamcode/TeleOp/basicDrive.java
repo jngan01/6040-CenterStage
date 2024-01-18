a1007497123d626c8714a5c8b3c9746095a72a8c
jngan01 <116775528+jngan01@users.noreply.github.com>
2024-01-17 23:00:03 -0500

    Update basicDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/basicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/basicDrive.java
@@ -14,10 +14,15 @@ public class basicDrive extends LinearOpMode {
     private DcMotor rf;
     private DcMotor rb;
 
-    private DcMotor hang;
-    private CRServo hangClaw;
-
+    private DcMotor arm;
+    private Servo rotator;
+    private Servo wrist;
+    private CRServo clamp;
     private CRServo drone;
+    private CRServo leftIntake;
+    private CRServo rightIntake;
+
+    private int armPos;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -27,10 +32,24 @@ public class basicDrive extends LinearOpMode {
         rf = hardwareMap.get(DcMotor.class, "rf");
         rb = hardwareMap.get(DcMotor.class, "rb");
 
-        hang = hardwareMap.get(DcMotor.class, "hang");
-        hangClaw = hardwareMap.get(CRServo.class, "hangClaw");
-
+        arm = hardwareMap.get(DcMotor.class, "arm");
+        rotator = hardwareMap.get(Servo.class, "rotator");
+        wrist = hardwareMap.get(Servo.class, "wrist");
+        clamp = hardwareMap.get(CRServo.class, "clamp");
         drone = hardwareMap.get(CRServo.class, "drone");
+        leftIntake = hardwareMap.get(CRServo.class, "leftIntake");
+        rightIntake = hardwareMap.get(CRServo.class, "rightIntake");
+
+
+
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER );
+
+        armPos = 0;
+        wrist.setPosition(0.05);
+        rotator.setPosition(0);
+        boolean isOpen = false;
+
 
         lf.setDirection(DcMotor.Direction.REVERSE);
         lb.setDirection(DcMotor.Direction.REVERSE);
@@ -40,7 +59,7 @@ public class basicDrive extends LinearOpMode {
         rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        hang.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         waitForStart();
 
@@ -71,6 +90,98 @@ public class basicDrive extends LinearOpMode {
                 rf.setPower(-1 * frontRightPower);
                 rb.setPower(-1 * backRightPower);
             }
+
+            // Orient pixels
+            if(gamepad2.dpad_left){
+                rotator.setPosition(.4);
+            } else if(gamepad2.dpad_up){
+                rotator.setPosition(0);
+            } else if(gamepad2.dpad_right){
+                rotator.setPosition(.75);
+            }else if(gamepad2.dpad_down){
+                rotator.setPosition(.5);
+            }
+
+            // Launch Drone
+            if(gamepad2.x){
+                drone.setPower(1);
+            } else{
+                drone.setPower(0);
+            }
+
+            //Intake toggle
+            if(gamepad2.y && isOpen){
+
+                leftIntake.setPower(.5);
+                rightIntake.setPower(-.5);
+                isOpen = false;
+            } else if(gamepad2.y && !isOpen) {
+                leftIntake.setPower(-.5);
+                rightIntake.setPower(.5);
+                isOpen = true;
+            }
+
+            // Pixel release failsafe
+            if(gamepad2.left_bumper){
+                clamp.setPower(.5);
+            }
+            if(gamepad2.right_bumper){
+                clamp.setPower(-.5);
+            }
+
+            // To adjust claw angle when placing on the Backdrop
+            if(gamepad2.left_trigger > 0){
+
+                wrist.setPosition(.4);
+            }
+            if(gamepad2.right_trigger > 0){
+
+                wrist.setPosition(.6);
+            }
+
+
+            // Reset the arm and claw positions
+            if(gamepad2.b){
+
+                clamp.setPower(1);
+                sleep(250);
+                rotator.setPosition(0);
+                sleep(250);
+                wrist.setPosition(.05);
+
+                //Pick up pixel and bring up.
+            } else if(gamepad2.a){
+
+                arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER );
+                clamp.setPower(-1);
+                sleep(500);
+                moveArm(-100, .35);
+                sleep(500);
+                wrist.setPosition(1);
+
+                //When not in autonomous mode, the arm can be controlled manually for more specific heights.
+            } else if(!gamepad2.a && !gamepad2.b){
+
+                arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER );
+                arm.setPower(gamepad1.left_stick_x * .5);
+            }
+        }
+
+    }
+    //Move arm to target position at a set speed. Once target position is reached, revert controls back to driver.
+    private void moveArm(int armTarget, double speed) {
+
+        armPos += armTarget;
+
+        arm.setTargetPosition(armPos);
+
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        arm.setPower(speed);
+
+
+        while (opModeIsActive() && arm.isBusy()) {
+            idle();
         }
 
 
